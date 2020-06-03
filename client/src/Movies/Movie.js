7c2575a6fbbb912e772f32cdd2cb15903586b89d
Darren Tebo <darrentebo83@outlook.com>
2020-06-03 16:22:11 -0400

    refactored movie and movielist by moving repetitive moviecard code into its own component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-const Movie = ({movie1}) => {
+import MovieCard from './MovieCard';
+
+const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   const params = useParams();
@@ -33,26 +35,9 @@ const Movie = ({movie1}) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-
   return (
     <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+      <MovieCard movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
