5092b6cb6e39744388373f74ef087ad4006ce25c
Darren Tebo <darrentebo83@outlook.com>
2020-06-03 16:38:33 -0400

    updated saved list links to navlinks and set the active style

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`}>
-        <span className="saved-movie">{movie.title}</span>
-      </Link>
+      <NavLink to={`/movies/${movie.id}`} activeClassName="saved-active">
+        <span key={movie.id} className="saved-movie">{movie.title}</span>
+      </NavLink>
     ))}
     <div className="home-button">
       <Link to="/">Home</Link>
